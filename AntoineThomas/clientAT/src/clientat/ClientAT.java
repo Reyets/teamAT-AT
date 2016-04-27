c3bb92c82e37a6e2a82c62f74c61106d6e15cdc6
Antoine STEYER <antoine.steyer@etu.unice.fr>
2016-04-27 17:09:02 +0200

    Premier client

--- AntoineThomas/clientAT/src/clientat/ClientAT.java
+++ AntoineThomas/clientAT/src/clientat/ClientAT.java
@@ -5,8 +5,10 @@
  */
 package clientat;
 
+import java.io.*;
+import java.net.*;
+
 /**
- *
  * @author chronos
  */
 public class ClientAT {
@@ -15,7 +17,18 @@ public class ClientAT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Socket smtpSocket = null;      // le socket client
+        DataOutputStream os = null; // output stream
+        DataInputStream is = null;    // input stream
+        try {
+            smtpSocket = new Socket("hostname", 9999);
+            os = new DataOutputStream(smtpSocket.getOutputStream());
+            is = new DataInputStream(smtpSocket.getInputStream());
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host: hostname");
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to: hostname");
+        }
     }
-    
 }
+
