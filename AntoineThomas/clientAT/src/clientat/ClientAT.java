38edd2b8819fa5b780dc93ef50ca76ca7c74b802
Antoine STEYER <antoine.steyer@etu.unice.fr>
2016-05-17 15:07:18 +0200

    Commande PARTICIPE

--- AntoineThomas/clientAT/src/clientat/ClientAT.java
+++ AntoineThomas/clientAT/src/clientat/ClientAT.java
@@ -7,6 +7,8 @@ package clientat;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
+
 import org.json.simple.JSONObject;
 import requetesmodele.Idee;
 
@@ -15,43 +17,91 @@ import requetesmodele.Idee;
  */
 public class ClientAT {
 
+
+    private JSONObject commande;
+
     public static void main(String[] zero) {
+        ClientAT client = new ClientAT();
         Socket socks;
         BufferedReader in;
         PrintWriter out;
-        
-        try {
-            socks = new Socket("localhost",9999);
-            System.out.println("Connecté");            
-            out = new PrintWriter(socks.getOutputStream());
-            Idee idee = new Idee();
-            JSONObject o = idee.toJSON();
 
-            out.println(o.toString());
+        try {
+            socks = new Socket("localhost", 9999);
+            System.out.println("Connecté");
+            out = new PrintWriter(socks.getOutputStream());
+            while (client.getSaisie()) {
+                out.println(client.getCommande().toString());
+                out.flush();
+                in = new BufferedReader(new InputStreamReader(socks.getInputStream()));
+                String message = in.readLine();
+                System.out.println(message);
+            }
+            out.println("quit");
             out.flush();
-            in = new BufferedReader(new InputStreamReader(socks.getInputStream()));
-            String message = in.readLine();
-            System.out.println(message);
             socks.close();
-        }catch (UnknownHostException e) {
-            e.printStackTrace();
-        }catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
-    private JSONObject getAction(String ask) throws Exception {
-        JSONObject action = new JSONObject();
-        if(ask.equalsIgnoreCase("list")) {
-            action.put("action", "LIST");
-        } else if(ask.equalsIgnoreCase("participe")) {
-            action.put("action", "PARTICIPE");
-        } else if(ask.equalsIgnoreCase("add")) {
-            action.put("action", "ADD");
-        } else {
-            throw new Exception("Unknown action");
+
+    private void printhelp() {
+        System.out.println("Commandes disponibles : ");
+        System.out.println("\t- add");
+        System.out.println("\t- participe");
+        System.out.println("\t- quit");
+        System.out.println();
+    }
+
+    private boolean getSaisie() {
+        printhelp();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Entrez votre commande : ");
+        switch (scan.nextLine()) {
+            case "add":
+                commande = creerIdee();
+                break;
+            case "participe":
+                commande = creerParticipation();
+                break;
+            case "help" :
+                printhelp();
+                break;
+            case "quit":
+                return false;
         }
-        return action;
+
+        return true;
+
+    }
+
+    private JSONObject creerIdee() {
+        Idee idee = new Idee();
+        return idee.toJSON();
+    }
+
+    private JSONObject creerParticipation() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("----| Participation |----");
+        System.out.println("Saisissez l'identifiant de l'idée : ");
+        int id = scan.nextInt();
+        System.out.println("Saisissez votre adresse email : ");
+        String mail = scan.nextLine();
+
+        JSONObject result = new JSONObject();
+        JSONObject idee = new JSONObject();
+        idee.put("identifiant",id);
+        idee.put("mail",mail);
+        result.put("idee",idee);
+        result.put("request","PARTICIPE");
+
+        return result;
+    }
+
+
+    private JSONObject getCommande() {
+        return commande;
     }
 
 }
